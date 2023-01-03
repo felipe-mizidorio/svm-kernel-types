9e220b328cd301b8fd1a879b26910853ee23715d
Felipe Merenda Izidorio <felipemerenda1@gmail.com>
2023-01-03 19:32:08 -0300

    feat: store the kernel results in a list

--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ from sklearn.metrics import accuracy_score
 
 with open('data/diabetes.csv', 'rb') as f:
     data_base = pd.read_csv(f)
+    
+print(data_base.shape)
 
 x_diabetes = data_base.iloc[:, 1:7].values
 y_diabetes = data_base.iloc[:, 8].values
@@ -15,8 +17,13 @@ x_diabetes = scaller_diabetes.fit_transform(x_diabetes)
 
 x_diabetes_training, x_diabetes_test, y_diabetes_training, y_diabetes_test = train_test_split(x_diabetes, y_diabetes, test_size=0.2, random_state=0)
 
-svm_diabetes = SVC(C=2.0, kernel='linear', random_state=1) # kernel = 'linear', 'poly', 'rbf', 'sigmoid', 'precomputed'
-svm_diabetes.fit(x_diabetes_training, y_diabetes_training)
-predictions = svm_diabetes.predict(x_diabetes_test)
+kernel_list = ['linear', 'poly', 'rbf', 'sigmoid']
+results = []
 
-print(accuracy_score(y_diabetes_test, predictions))
+for kernel in kernel_list:
+    svm_diabetes = SVC(C=2.0, kernel=kernel, random_state=1)
+    svm_diabetes.fit(x_diabetes_training, y_diabetes_training)
+    predictions = svm_diabetes.predict(x_diabetes_test)
+    results.append(accuracy_score(y_diabetes_test, predictions))
+    
+print(results)
