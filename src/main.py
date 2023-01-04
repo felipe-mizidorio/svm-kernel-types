7180367077da97cd39d470d59972de26849cb8de
Felipe Merenda Izidorio <felipemerenda1@gmail.com>
2023-01-04 14:51:50 -0300

    feat: implement accuracy score graph of svm

--- src/main.py
+++ src/main.py
@@ -3,11 +3,10 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
 
 with open('data/diabetes.csv', 'rb') as f:
     data_base = pd.read_csv(f)
-    
-print(data_base.shape)
 
 x_diabetes = data_base.iloc[:, 1:7].values
 y_diabetes = data_base.iloc[:, 8].values
@@ -24,6 +23,12 @@ for kernel in kernel_list:
     svm_diabetes = SVC(C=2.0, kernel=kernel, random_state=1)
     svm_diabetes.fit(x_diabetes_training, y_diabetes_training)
     predictions = svm_diabetes.predict(x_diabetes_test)
-    results.append(accuracy_score(y_diabetes_test, predictions))
+    results.append(accuracy_score(y_diabetes_test, predictions)*100)
     
-print(results)
+fig, ax = plt.subplots()
+ax.bar(kernel_list, results)
+ax.set_ylim(min(results)-5, max(results)+5)
+ax.set_title('SVM - Accuracy Score of different kernel types')
+ax.set_ylabel('Accuracy(%)')
+ax.set_xlabel('Kernel types')
+plt.show()
